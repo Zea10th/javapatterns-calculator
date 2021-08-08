4200cfc9a77bc9c6c7dac34b13ac3b002177ae37
Alexander Biezbardis <mr.biezbardis@gmail.com>
2021-08-08 21:54:41 +0300

    Added realization and tests

--- src/main/java/org/example/IntsCalculator.java
+++ src/main/java/org/example/IntsCalculator.java
@@ -9,20 +9,32 @@ public class IntsCalculator implements Ints {
 
     @Override
     public int sum(int arg0, int arg1) {
-        //считаем через target
-        return 0;
+        int result = (int) target.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(Calculator.Operation.SUM)
+                .result();
+        return result;
     }
 
     @Override
     public int mult(int arg0, int arg1) {
-        //считаем через target
-        return 0;
+        int result = (int) target.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(Calculator.Operation.MULT)
+                .result();
+        return result;
     }
 
     @Override
     public int pow(int a, int b) {
-        //считаем через target
-        return 0;
+        int result = (int) target.newFormula()
+                .addOperand(a)
+                .addOperand(b)
+                .calculate(Calculator.Operation.POW)
+                .result();
+        return result;
     }
 }
 
--- src/test/java/org/example/IntsCalculatorTest.java
+++ src/test/java/org/example/IntsCalculatorTest.java
@@ -1,20 +1,45 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class IntsCalculatorTest {
 
-    @Test
-    void sum() {
+    @ParameterizedTest
+    @CsvSource({"1,7", "2,6", "3,5", "4,4"})
+    void shouldReturnSum(int a, int b) {
+        Ints calc = new IntsCalculator();
+
+        int expected = 8;
+        int actual = calc.sum(a, b);
+
+        assertEquals(expected, actual);
     }
 
-    @Test
-    void mult() {
+    @ParameterizedTest
+    @CsvSource({"1,18", "2,9", "3,6"})
+    void shouldReturnMult(int a, int b) {
+        Ints calc = new IntsCalculator();
+
+        int expected = 18;
+        int actual = calc.mult(a, b);
+
+        assertEquals(expected, actual);
     }
 
-    @Test
-    void pow() {
+    @ParameterizedTest
+    @CsvSource({"81,2", "9,4", "3,8"})
+    void shouldReturnPow(int a, int b) {
+        Ints calc = new IntsCalculator();
+
+        int expected = 6_561;
+        int actual = calc.pow(a, b);
+
+        assertEquals(expected, actual);
     }
 }
